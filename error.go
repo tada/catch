c7c2fd03001c04d2f0e800dbd5ee6e173b93e7a0
Thomas Hallgren <thomas@tada.se>
2020-04-14 09:58:21 +0200

    Make Error function accept argument combinations.
    This commit makes it possible to call the `Error()` function, not just
    with an error, but with a string and arguments. Internally, an error
    will be used verbatim or created based on the arguments with either
    `errors.New()` or `fmt.Errorf()`.

--- error.go
+++ error.go
@@ -1,5 +1,10 @@
 package catch
 
+import (
+	"errors"
+	"fmt"
+)
+
 // special error that is recovered by the Do() function returning its original cause.
 type errorWithCause struct {
 	// Cause is the original error
@@ -8,8 +13,36 @@ type errorWithCause struct {
 
 // Error returns a new special error object that wraps the given cause. The returned error can be used as an
 // argument to the IsError() and Cause() functions.
-func Error(cause error) error {
-	return &errorWithCause{cause: cause}
+//
+// One argument
+//
+// The argument can be either an error or a string. An error will used verbatim whereas a string will be
+// converted using the function errors.New().
+//
+// Multiple arguments
+//
+// The first argument must be a string. If that is not the case, an illegal argument panic is raised. The
+// string and the remaining arguments are passed to the function fmt.Errorf() to create an error.
+func Error(cause ...interface{}) error {
+	var err error
+	if nc := len(cause); nc > 0 {
+		switch a1 := cause[0].(type) {
+		case error:
+			if nc == 1 {
+				err = &errorWithCause{cause: a1}
+			}
+		case string:
+			if nc == 1 {
+				err = &errorWithCause{cause: errors.New(a1)}
+			} else {
+				err = &errorWithCause{cause: fmt.Errorf(a1, cause[1:]...)}
+			}
+		}
+	}
+	if err != nil {
+		return err
+	}
+	panic(errors.New("catch.Error(): illegal argument"))
 }
 
 // Error returns the result of calling Error() on the contained cause
--- error_test.go
+++ error_test.go
@@ -23,3 +23,40 @@ func TestError(t *testing.T) {
 		t.Error("Cause() does not return nil on unknown errors")
 	}
 }
+
+func TestError_string(t *testing.T) {
+	ex := catch.Error("the error")
+	if ex.Error() != "the error" {
+		t.Error("Error() created error produces wrong error string")
+	}
+	if !catch.IsError(ex) {
+		t.Error("IsError() returns false for Error() created error ")
+	}
+}
+
+func TestError_fmt(t *testing.T) {
+	ex := catch.Error("the numbers %d and %d are illegal", 38, 70)
+	if ex.Error() != "the numbers 38 and 70 are illegal" {
+		t.Error("Error() created error produces wrong error string")
+	}
+}
+
+func TestError_noArgs(t *testing.T) {
+	defer func() {
+		err := recover()
+		if err == nil || err.(error).Error() != "catch.Error(): illegal argument" {
+			t.Errorf("expected illegal argument panic")
+		}
+	}()
+	_ = catch.Error()
+}
+
+func TestError_badArgs(t *testing.T) {
+	defer func() {
+		err := recover()
+		if err == nil || err.(error).Error() != "catch.Error(): illegal argument" {
+			t.Errorf("expected illegal argument panic")
+		}
+	}()
+	_ = catch.Error(errors.New("oops"), "bad additional arg")
+}
